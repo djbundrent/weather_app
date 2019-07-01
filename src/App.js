9911d2ecab8e0b898f332007253c81f26e4b4a5f
Devin Bundrent <devinbundrent@icloud.com>
2019-07-01 17:49:30 -0400

    fetch current weather data from an api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import Titles from './components/titles';
 import Form from './components/form';
 import Weather from './components/weather';
 
+const API_KEY = "d73cd78ea5ced8d289bf46b308195fe8";
 class App extends Component {
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}`);
+
+    const data = await api_call.json();
+
+    console.log(data);
+  }
+
   render(){
     return (
       <div>
