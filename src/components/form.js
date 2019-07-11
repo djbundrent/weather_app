b058b3bd5927dddfc7b8d0c9dc68b98af1dad2d1
Devin Bundrent <devinbundrent@icloud.com>
2019-07-11 17:00:36 -0400

    add cosmetic changes and 'Get Weather' button

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Form extends Component {
-    render() {
-        return(
-            <form> 
-                <input type="text" name="city" placeholder="City..." />
-                <input type="text" name="country" placeholder="Country..." />
-                <button>Get Weather</button>     
-            </form>
-        )
-    }
+  render () {
+    return (
+      <form onSubmit={this.props.getWeather}>
+        <input type='text' name='city' placeholder='City...' />
+        <input type='text' name='country' placeholder='Country...' />
+        <button>Get Weather</button>
+      </form>
+    )
+  }
 }
 
-export default Form;
+export default Form
